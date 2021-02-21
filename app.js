5aa4d99ec48bd713b84a7ad577536d9421312742
Salimyaro <3508880@i.ua>
2021-02-21 21:45:46 +0200

    done

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
 
-const contactsRouter = require('./routes/api/contacts')
+import contactsRouter from './routes/api/contacts.js'
 
+const { json } = express;
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   res.status(500).json({ message: err.message })
 })
 
-module.exports = app
+export default app
