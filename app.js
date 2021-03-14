eeff618215e119be7f2ff2f73dacc12b436759ac
Salimyaro <3508880@i.ua>
2021-03-14 23:39:50 +0200

    Added the ability to download and update user avatars via Multer

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-import express from 'express'
-import logger from 'morgan'
-import cors from 'cors'
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import { HttpCode } from "./helpers/constants.js";
 import contactsRouter from "./routes/api/contacts/index.js";
 import usersRouter from "./routes/api/users/index.js";
-
 const { json } = express;
-const app = express()
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
+app.use(express.static(process.env.PUBLIC_DIR));
 app.use(helmet());
-app.use(logger(formatsLogger))
-app.use(cors())
+app.use(logger(formatsLogger));
+app.use(cors());
 app.use(json({ limit: 10000 }));
 
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000,
   max: 100,
-  handler: (req, res, next) => {
+  handler: (_req, res, _next) => {
     return res.status(HttpCode.BAD_REQUEST).json({
       status: "error",
       code: HttpCode.BAD_REQUEST,
@@ -30,16 +29,18 @@ const apiLimiter = rateLimit({
   },
 });
 
-app.use('/api/', apiLimiter);
-app.use('/api/users', usersRouter);
-app.use('/api/contacts', contactsRouter)
+app.use("/api/", apiLimiter);
+app.use("/api/users", usersRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((_req, res) => {
   res.status(HttpCode.NOT_FOUND).json({ message: "Not found" });
-})
+});
 
 app.use((err, _req, res, _next) => {
-  res.status(err.status || HttpCode.INTERNAL_SERVER_ERROR).json({ message: err.message });
-})
+  res
+    .status(err.status || HttpCode.INTERNAL_SERVER_ERROR)
+    .json({ message: err.message });
+});
 
-export default app
+export default app;
